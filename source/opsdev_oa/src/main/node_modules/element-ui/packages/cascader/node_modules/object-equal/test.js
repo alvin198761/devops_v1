be87a51a6d03860d46c0b37bf1f0605f3aa45ca9
月球王子 <alvin198761@163.com>
2017-03-21 22:51:05 +0800

    删除文件

--- source/opsdev_oa/src/main/node_modules/element-ui/packages/cascader/node_modules/object-equal/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var equals = require('./index');
-
-assert(equals({a:1, b:'nada'}, {b:'nada', a:1}));
-assert(!equals({a:1, b:'nada'}, {b:'nada', a:2}));
-assert(!equals({a:1, b:''}, {b:'nada', a:1}));
-assert(!equals({a:1, b:'nada'}, {a:1}));
-assert(!equals({a:1}, {a:1, b:'nada'}));
